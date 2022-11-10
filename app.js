0c4328caaacd7556b835f9b198b6c545df3a1638
coddy83 <coddy083@gmail.com>
2022-11-10 11:28:47 +0900

    fix websocket emit

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const helloWorld = async () => {
+  await console.log("Hello World");
+}
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
 io.on("connection", (socket) => {
+  helloWorld();
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
+    socket.broadcast.emit("chat message", msg);
   });
   //send message to client
-  socket.emit("chat message", "Hello from server");
 });
 
 server.listen(4005, () => {
