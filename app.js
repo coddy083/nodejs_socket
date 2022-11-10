5f1016595077107f9bf6afa9bd421b3f4873596a
coddy83 <coddy083@gmail.com>
2022-11-10 19:21:09 +0900

    fix socket emit update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const app = express();
 const http = require("http");
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
 const helloWorld = async () => {
   await console.log("Hello World");
-}
+};
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
@@ -20,10 +20,16 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    console.log("chat message: " + msg);
     socket.broadcast.emit("chat message", msg);
   });
-  //send message to client
+  socket.on("Welcome", (msg) => {
+    console.log("Welcome: " + msg);
+    socket.broadcast.emit("Welcome", msg);
+  });
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
 });
 
 server.listen(4005, () => {
