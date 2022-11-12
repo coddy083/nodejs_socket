f9261719ca2ae8502ff03e7d0b2c6e81e6db65ed
coddy83 <coddy083@gmail.com>
2022-11-12 20:47:50 +0900

    fix app

--- app.js
+++ app.js
@@ -100,8 +100,8 @@ chat.on("connection", (socket) => {
   socket.on("chat message", (room, message) => {
     console.log(message);
     // chat.to(room).emit("message", message);
-    chat.in(room).emit("chat message", message);
-    // chat.emit("message", message);
+    // chat.in(room).emit("chat message", message);
+    chat.emit("message", message);
   }
   );
 });
