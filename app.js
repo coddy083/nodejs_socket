ae4370edcd783eba71da7fe7f6fe0c7952687553
coddy83 <coddy083@gmail.com>
2022-11-12 18:12:07 +0900

    fix app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const { Server } = require("socket.io");
-const { ObjectId } = require("mongodb");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// const { ObjectId } = require("mongodb");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-const bodyParser = require("body-parser").json();
+// const bodyParser = require("body-parser").json();
 const http = require("http");
 const app = express();
 const port = process.env.PORT || 4005;
 const server = http.createServer(app);
 const io = new Server(server);
-const uri =
-  "mongodb+srv://user:10041004@cluster0.avef3.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: { version: ServerApiVersion.v1 },
-});
-client.connect((err) => {
-  const collection = client.db("UserDB").collection("User");
-  // perform actions on the collection object
-});
+// const uri =
+//   "mongodb+srv://user:10041004@cluster0.avef3.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: { version: ServerApiVersion.v1 },
+// });
+// client.connect((err) => {
+//   const collection = client.db("UserDB").collection("User");
+//   // perform actions on the collection object
+// });
 
-const UserDB = client.db("UserDB").collection("User");
+// const UserDB = client.db("UserDB").collection("User");
 
 // const interval = setInterval(() =>{
 //   console.log("Hello World");
@@ -28,7 +28,7 @@ const UserDB = client.db("UserDB").collection("User");
 
 app.get("/", (req, res) => {
   // interval;
-  res.status(200).json(database);
+  res.status(200).json({"msg" : "hello world"});
 });
 
 // app.get("/users", (req, res) => {
@@ -38,45 +38,45 @@ app.get("/", (req, res) => {
 //   });
 // });
 
-app.get("/users", async (req, res) => {
-  req.query.id
-    ? await UserDB.findOne({ _id: ObjectId(req.query.id) }, (err, result) => {
-        if (err) throw err;
-        res.status(200).json(result);
-      })
-    : await UserDB.find({}).toArray((err, result) => {
-        if (err) throw err;
-        res.status(200).json(result);
-      });
-});
+// app.get("/users", async (req, res) => {
+//   req.query.id
+//     ? await UserDB.findOne({ _id: ObjectId(req.query.id) }, (err, result) => {
+//         if (err) throw err;
+//         res.status(200).json(result);
+//       })
+//     : await UserDB.find({}).toArray((err, result) => {
+//         if (err) throw err;
+//         res.status(200).json(result);
+//       });
+// });
 
-app.post("/users", bodyParser, (req, res) => {
-  const { name, age } = req.body;
-  UserDB.insertOne({ name, age }, (err, result) => {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
-});
+// app.post("/users", bodyParser, (req, res) => {
+//   const { name, age } = req.body;
+//   UserDB.insertOne({ name, age }, (err, result) => {
+//     if (err) throw err;
+//     res.status(200).json(result);
+//   });
+// });
 
-app.put("/users", bodyParser, (req, res) => {
-  const { id, name, age } = req.body;
-  UserDB.updateOne(
-    { _id: ObjectId(id) },
-    { $set: { name, age } },
-    (err, result) => {
-      if (err) throw err;
-      res.status(200).json(result);
-    }
-  );
-});
+// app.put("/users", bodyParser, (req, res) => {
+//   const { id, name, age } = req.body;
+//   UserDB.updateOne(
+//     { _id: ObjectId(id) },
+//     { $set: { name, age } },
+//     (err, result) => {
+//       if (err) throw err;
+//       res.status(200).json(result);
+//     }
+//   );
+// });
 
-app.delete("/users", bodyParser, (req, res) => {
-  const { id } = req.body;
-  UserDB.deleteOne({ _id: ObjectId(id) }, (err, result) => {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
-});
+// app.delete("/users", bodyParser, (req, res) => {
+//   const { id } = req.body;
+//   UserDB.deleteOne({ _id: ObjectId(id) }, (err, result) => {
+//     if (err) throw err;
+//     res.status(200).json(result);
+//   });
+// });
 
 const chat = io.of("/chat");
 
