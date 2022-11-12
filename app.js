abf140ffd1314d748cbe1b16a9b788faba9e2efe
coddy83 <coddy083@gmail.com>
2022-11-12 20:29:13 +0900

    asdasd

--- app.js
+++ app.js
@@ -99,7 +99,9 @@ chat.on("connection", (socket) => {
   });
   socket.on("message", (room, message) => {
     console.log(message);
-    chat.to(room).emit("message", message);
+    // chat.to(room).emit("message", message);
+    // chat.in(room).emit("message", message);
+    chat.emit("message", message);
   }
   );
 });
