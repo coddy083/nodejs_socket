71e093f7cd9093361186434cd01b715d6457515b
coddy83 <coddy083@gmail.com>
2022-11-14 15:12:53 +0900

    asdasd

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.delete("/users", bodyParser, (req, res) => {
 
 app.get("/chatList" , (req, res) => {
   //last 10 chat reserved
-  chatDB.find({}).sort({_id:1}).limit(10).toArray((err, result) => {
+  chatDB.find({}).sort({_id:-1}).limit(20).sort({_id:1}).toArray((err, result) => {
     if (err) throw err;
     res.status(200).json(result);
   });
