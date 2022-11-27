9e7b970029cce686404b65dc135ba779c68ac66d
coddy083 <coddy083@gmail.com>
2022-11-27 13:54:55 +0900

    fix app modules

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const { Server } = require("socket.io");
-const express = require("express");
-const uri = 'mongodb://root:1234@mongodb:27017';
-const http = require("http");
+import { Server } from 'socket.io';
+import { hello } from './hello.js';
+import { chatSave, Chat } from './serializer.js';
+import express from 'express';
+import http from 'http';
 const app = express();
 const port = process.env.PORT || 8008;
 const server = http.createServer(app);
@@ -12,45 +13,15 @@ server.listen(port, () => {
   console.log(`서버시작 포트는 ${port}`);
 });
 
-// 몽구스 연결
-const mongoose = require("mongoose");
-mongoose.connect(uri,{
-      // useNewUrlPaser: true,
-      // useUnifiedTofology: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    }
-  )
-  .then(() => console.log('***** MongoDB conected *****'))
-  .catch((err) => {
-    console.log(err);
-  });
-
-// 스키마 생성
-const Schema = mongoose.Schema;
-const chatMessage = new Schema({
-  user: String,
-  room: String,
-  userName: String,
-  message: String,
-  time: Date,
-});
-
-// 모델 생성
-const Chat = mongoose.model('Chat', chatMessage);
-
-// 데이터 생성
-async function chatSave(chatDoc) {
-  const chatmsg = new Chat(chatDoc);
-  chatmsg.save((err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-};
-// 몽구스 종료
-
 app.get("/", async (req, res) => {
-  res.status(200).json({ msg: "Server Moa Gudok Chat Server" });
+  chatSave({
+    user: "user",
+    room: "room1",
+    userName: "user1",
+    message: "hello world",
+    time: new Date(),
+  });
+  res.status(200).json({ msg: hello() });
 });
 
 app.get("/chatList", (req, res) => {
